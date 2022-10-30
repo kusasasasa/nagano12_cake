a25706457fa540e61296be0c5e95c29d5246a110
EC2 Default User <ec2-user@ip-172-31-23-26.ap-northeast-1.compute.internal>
2022-10-30 00:35:31 +0000

    Initial Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,26 @@
 class ApplicationController < ActionController::Base
+            protect_from_forgery with: :exception
+
+            # ログイン済ユーザーのみにアクセスを許可する
+            before_action :authenticate_user!
+          
+            # deviseコントローラーにストロングパラメータを追加する          
+            before_action :configure_permitted_parameters, if: :devise_controller?
+
+            
+            
             def after_sign_in_path_for(resource)
                 case resource
                 when Customer
-                  user_posts_path
+                  public_homes_top_url
                 when Admin
-                  admin_user_posts_path
+                  admin_homes_top_url
                 end
+            end
+            protected
+
+            def configure_permitted_parameters
+              # デフォルトにないname/join_year/部署を追加しているので、それらを許可するよう記載している
+              devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:join_year,:department_id])
             end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :customers,skip: [:passwords], controllers: {
+  
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    
+    get 'homes/top'
+    get 'homes/about'
+  end
+
+ devise_for :customers, controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
-  }
-  
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
+ 
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ root 'public/homes#top'
 end
