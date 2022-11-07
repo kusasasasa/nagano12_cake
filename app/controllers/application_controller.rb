e7c5e5477883c3a2d54538cdae3c2a0d64f00e3b
EC2 Default User <ec2-user@ip-172-31-23-26.ap-northeast-1.compute.internal>
2022-11-07 09:59:10 +0000

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,23 @@
 class ApplicationController < ActionController::Base
             protect_from_forgery with: :exception
 
-            # ログイン済ユーザーのみにアクセスを許可する
-            before_action :authenticate_user!
-          
-            # deviseコントローラーにストロングパラメータを追加する          
+                   
             before_action :configure_permitted_parameters, if: :devise_controller?
 
             
             
             def after_sign_in_path_for(resource)
                 case resource
-                when Customer
-                  public_homes_top_url
-                when Admin
-                  admin_homes_top_url
+                  when Customer
+                    public_homes_top_path
+                  when Admin
+                    admin_homes_top_path
                 end
             end
-            protected
+            private
 
             def configure_permitted_parameters
-              # デフォルトにないname/join_year/部署を追加しているので、それらを許可するよう記載している
-              devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:join_year,:department_id])
+
+              devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number])
             end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
     get 'items/index'
     get 'items/show'
   end
