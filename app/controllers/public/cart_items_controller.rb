8cb853108706847ef01ea50951613ec927081b94
EC2 Default User <ec2-user@ip-172-31-23-26.ap-northeast-1.compute.internal>
2022-12-01 11:24:00 +0000

    Initiat

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,24 +9,35 @@ class Public::CartItemsController < ApplicationController
    
   end
   def create
-  
-    cart_item = CartItem.new(cart_item_params)
-  
-    if current_customer.cart_items.find_by(item_id: cart_item.item_id)
+   
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present? # 追加しようとしている商品がcartにあるかどうか?
         
-        cart_item.save
+        # 追加しようとしている商品が存在した場合は,存在するrecordのamountの値を追加する
+        
+        # 1. すでにcartに存在するitemの個数を定義する(既に存在するrecoredを取得->個数を引き出す)
+        old_amount = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).amount
+        
+        # 2. 新しく追加しようとしている数の定義
+        amount = params[:cart_item][:amount]
+        
+        # 3.　1,2をそれぞれ足して既存のcartのrecordを更新する
+        # 更新をかけるrecordを定義
+        cart_item=current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+        
+        # 更新をかけよう(更新を掛けたい対象.update(カラム名: value))
+        cart_item.update(amount: old_amount.to_i+amount.to_i)
         redirect_to public_cart_items_index_path
     else
+        cart_item = CartItem.new(cart_item_params)
         
         cart_item.customer_id = current_customer.id
        
-        cart_item.save
+        cart_item.save!
           # 4. トップ画面へリダイレクト
         redirect_to public_cart_items_index_path
     end
-    
-  
   end
+  
   def update
     @cart_item=CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
